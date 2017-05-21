680228e6b989dd9d362d71834288c36da52ecb7e
Maxim Vorontsov <maraquee@icloud.com>
2017-05-21 19:14:35 +0300

    3-commit *add jquery *add magnific-popup *add concat *add uglify

---
+++ app/js/common.js
@@ -0,0 +1,6 @@
+/**
+ * Created by maraq on 21.05.2017.
+ */
+$(function () {
+    
+});
--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,25 @@
  */
 var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
-    browserSync = require('browser-sync'); // Connect Saas pack
+    browserSync = require('browser-sync'),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglifyjs');
 
 gulp.task('sass', function () {
     return gulp.src('app/sass/**/*.sass')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('scripts', function () {
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js/'));
 });
 
 gulp.task('browser-sync', function () {
@@ -20,6 +33,8 @@ gulp.task('browser-sync', function () {
     });
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', ['browser-sync', 'sass'], function () {
     gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch('app/**/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
 });

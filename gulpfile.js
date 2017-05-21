fc392bfd556a317b3f4217d9ff5b863dd8733440
Maxim Vorontsov <maraquee@icloud.com>
2017-05-21 17:25:09 +0300

    2-commit *add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 /**
  * Created by Максим on 13.05.2017.
  */
-var gulp = require('gulp');
-var sass= require('gulp-sass'); // Connect Saas pack
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync'); // Connect Saas pack
 
+gulp.task('sass', function () {
+    return gulp.src('app/sass/**/*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/sass/**/*.sass', ['sass']);
+});
